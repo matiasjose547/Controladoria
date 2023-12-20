3e67b1302f723d0c7f0902a9b5bd5bc7d2b9e5c2
Matias <matiasjose547@gmail.com>
2023-12-20 14:02:14 -0300

    added function to register new oficios

--- src/main/java/cgm/al/gov/br/app/controllers/OficioController.java
+++ src/main/java/cgm/al/gov/br/app/controllers/OficioController.java
@@ -1,21 +1,45 @@
 package cgm.al.gov.br.app.controllers;
 
+import cgm.al.gov.br.app.models.Oficio;
 import cgm.al.gov.br.app.services.OficioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.sql.SQLException;
+import java.util.List;
 
 @Controller
 @RequestMapping("oficios")
 public class OficioController {
     @Autowired
     private OficioService oficioService;
+    private List<Oficio> oficioList;
+
     @GetMapping
-    public ModelAndView getPage() {
+    public ModelAndView getList() {
         ModelAndView mv = new ModelAndView();
-        mv.addObject("oficioList", oficioService.findAll());
+        oficioList = oficioService.findAll();
+        mv.addObject("oficioList", oficioList);
         return mv;
     }
+    @PostMapping
+    public ModelAndView create(Oficio oficio){
+        ModelAndView mv = new ModelAndView();
+        try {
+            Oficio o = oficioService.save(oficio);
+            oficioList.add(o);
+            mv.addObject("oficioList", oficioList);
+            mv.addObject("message", "Oficio criado com sucesso!");
+        } catch (Exception e){
+            mv.addObject("message", "Erro ao criar oficio, verifique todas as informações correspondentes!");
+            e.printStackTrace();
+        } finally {
+            return mv;
+        }
+    }
+
 }
--- src/main/java/cgm/al/gov/br/app/repositories/OficioRepository.java
+++ src/main/java/cgm/al/gov/br/app/repositories/OficioRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface OficioRepository extends JpaRepository<Oficio, Long> {
-    List<Oficio> findAll();
+
 }
